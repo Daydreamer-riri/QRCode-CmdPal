ba4b39e6d75b63a314c99935f321e9d6fd9db4c2
Riri <Daydreamerriri@outlook.com>
2026-02-11 15:52:18 +0800

    chore: v0.0.2

--- CopyMsixFile.ps1
+++ CopyMsixFile.ps1
@@ -1,3 +1,4 @@
+Get-ChildItem -Path ./build -Recurse | Remove-Item -Force -Recurse
 mkdir build
 Get-ChildItem -Path ./ -Recurse -Include *.msix | Where-Object { $_.FullName -notmatch "\\Dependencies\\" } | ForEach-Object {
   Copy-Item -Path $_.FullName -Destination build
